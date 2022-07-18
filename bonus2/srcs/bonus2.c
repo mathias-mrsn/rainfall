d17a4a3eaf6a09e5d7100232250f1f55ebe8dcb4
Mathias MAURAISIN <mathias.mrsn@gmail.com>
2022-07-18 20:50:24 +0200

    bonus2 and bonus3 done

---
+++ bonus2/srcs/bonus2.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int	language;
+
+void
+greetuser (char *user) {
+	char	buffer[64];
+
+	if (language == 1)
+		strcpy(buffer, "Hyvää päivää ");
+	else if (language == 2)
+		strcpy(buffer, "Goedemiddag! ");
+	else if (language == 0)
+		strcpy(buffer, "Hello ");
+	strcat(buffer, user);
+	puts(buffer);
+}
+
+int
+main (int argc, char **argv) {
+	char	buffer[72];
+	char	*env;
+
+	env = NULL;
+	if (argc != 3) {
+		return (1);
+	}
+	memset(buffer, 0, 72);
+	strncpy(buffer, argv[1], 40);
+	strncpy(&buffer[40], argv[2], 32);
+	env = getenv("LANG");
+	if (env) {
+		if (memcmp(env, "fi", 2) == 0) {
+			language = 1;
+		} else if (memcmp(env, "nl", 2) == 0) {
+			language = 2;
+		}
+	}
+	greetuser(buffer);
+	return (0);
+}
